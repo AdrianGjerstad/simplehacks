3d7f347e836df27df3432a4ed1e3f99fc073b51b
Adrian Gjerstad <adgjerstad@gmail.com>
2019-12-03 14:43:29 +0000

    more

--- rot26.js
+++ rot26.js
@@ -1,12 +1,12 @@
-window.onclick = function() {
-  let string = prompt('What should I encrypt?')
+alert('foo')
+alert('foo')
+let string = prompt('What should I encrypt?')
 
-  let httpr = new XMLHttpRequest();
-  httpr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-      alert('Encrypted: ' + httpr.responseText)
-    }
+let httpr = new XMLHttpRequest();
+httpr.onreadystatechange = function() {
+  if(this.readyState == 4 && this.status == 200) {
+    alert('Encrypted: ' + httpr.responseText)
   }
-  httpr.open('GET', 'http://api.rot26.org/encrypt/' + string, true);
-  httpr.send();
 }
+httpr.open('GET', 'http://api.rot26.org/encrypt/' + string, true);
+httpr.send();
